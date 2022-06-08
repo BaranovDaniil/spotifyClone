c6806a62801044297b256dc9e9f1ba9f01640781
Daniil_Baranov <daniilbaranov2019@gmail.com>
2022-06-08 12:23:38 +0300

    Added error handling

--- public/fetchShows.js
+++ public/fetchShows.js
@@ -5,13 +5,14 @@ import { token } from "./getToken.js";
  * Fetch shows and display it on the website
  */
 async function fetchShows() {
-  let target = document.body.getElementsByClassName("main-view__my-shows")[0];
-  const apiToken = await token();
-  const shows = await getShows(apiToken);
-  for (let i = 0; i < shows.length; i++) {
-    target.insertAdjacentHTML(
-      "beforeend",
-      `<div class="my-shows__card">
+  try {
+    let target = document.body.getElementsByClassName("main-view__my-shows")[0];
+    const apiToken = await token();
+    const shows = await getShows(apiToken);
+    for (let i = 0; i < shows.length; i++) {
+      target.insertAdjacentHTML(
+        "beforeend",
+        `<div class="my-shows__card">
         <div class="card__image-container">
         <img
           loading="lazy"
@@ -27,7 +28,10 @@ async function fetchShows() {
             <span class="title-container__name-of-author">${shows[i].publisher}</span>
         </div>
        </div>`
-    );
+      );
+    }
+  } catch (error) {
+    alert(error);
   }
 }
 
